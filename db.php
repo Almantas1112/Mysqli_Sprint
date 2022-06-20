fc2474c5c36b419af517c7f35a250ba665dbff8a
Almantas1112 <aalmantass1112@gmail.com>
2022-06-20 17:10:00 +0300

    Adding db.php (mysqli connection config file)

---
+++ db.php
@@ -0,0 +1,13 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbName = "sprint";
+
+$conn = mysqli_connect($servername, $username, $password, $dbName);
+
+if(!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+// echo "Connected!";
